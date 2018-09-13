f0366c0fb18632db597e5fad5b2eceac326c96f9
Ashkan <kalantari@gmail.com>
2018-09-13 15:19:52 +0100

    added ordering and grouping for apps

--- api/models/Security/SecurityModel.js
+++ api/models/Security/SecurityModel.js
@@ -3,29 +3,39 @@ var db = require("../../../Utils/sqliteDb");
 exports.listOfAccessibleApps = function(username) {
   let query = "";
   if (username === "guest") {
-    query = `SELECT SA.Application as RtName
+    query = `SELECT SA.ApplicationID
+            ,SA.Application
             ,SA.RouteName as [Path]
             ,SA.Icon as Icon
             ,SA.Component as Component
             ,SA.Params as params
-             FROM tbSecApplication SA WHERE IsPublic = 1`;
+            ,SA.ParentID
+            ,SA.ShowInNavigationTree
+            ,IFNULL(SA.AppOrder,1000 + SA.ApplicationID) as AppOrder
+             FROM tbSecApplication SA WHERE IsPublic = 1
+             ORDER BY IFNULL(SA.AppOrder,1000 + SA.ApplicationID) `;
   } else {
     query = `SELECT
-        SA.Application as RtName
+        SA.ApplicationID
+        ,SA.Application
         ,SA.RouteName as [Path]
         ,SA.Icon as Icon
         ,SA.Component as Component
         ,SA.Params as params
+        ,SA.ParentID
+        ,SA.ShowInNavigationTree
+        ,IFNULL(SA.AppOrder,1000 + SA.ApplicationID) as AppOrder
         FROM tbSecApplication SA
         LEFT JOIN tbSecApplicationRole SAR ON SA.ApplicationID = SAR.ApplicationID
         LEFT JOIN tbSecUserRole UR ON UR.RoleID = SAR.RoleID
         LEFT JOIN tbSecUser U ON U.UserID = UR.UserID AND U.UserName LIKE '${username}'
-        WHERE (ISPublic = 1 OR U.UserID IS NOT NULL)`;
+        WHERE (ISPublic = 1 OR U.UserID IS NOT NULL)
+        ORDER BY IFNULL(SA.AppOrder,1000 + SA.ApplicationID) `;
   }
   return new Promise((resolve, reject) => {
     db.all(query, [], (err, rows) => {
       if (err) {
-        console.log(err);
+        //console.log(err);
         reject(err);
       } else {
         console.log(rows);
