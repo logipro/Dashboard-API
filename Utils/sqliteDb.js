8c3082bddb0b23aad168a8f3924cd801472a70a3
Ashkan <kalantari@gmail.com>
2018-10-01 09:22:12 +0100

    DB file name moved to config

--- Utils/sqliteDb.js
+++ Utils/sqliteDb.js
@@ -1,15 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
-
+const config = require("../config");
 // open the database
-let db = new sqlite3.Database(
-  "./Database/QuestSqlite.db",
-  sqlite3.OPEN_READWRITE,
-  err => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the Quest Sqlite database.");
+let db = new sqlite3.Database(config.sqliteDB, sqlite3.OPEN_READWRITE, err => {
+  if (err) {
+    console.error(err.message);
   }
-);
+  console.log("Connected to the Quest Sqlite database.");
+});
+//enabling foreign keys
+db.get("PRAGMA foreign_keys = ON");
 
 module.exports = db;
--- config.js
+++ config.js
@@ -2,4 +2,6 @@ var config = {};
 
 config.jwt_secret = "L0g!Pr0T0p$ecreT";
 
+config.sqliteDB = "./Database/LogiProSqlite";
+
 module.exports = config;
